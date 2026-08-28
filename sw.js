0cc1a4e4e158d345cc8cbebad303e3ab4b2effd5
jugal032021-alt <jugal032021@gmail.com>
2026-08-28 15:01:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,40 @@
+const CACHE_NAME = "pathasala-store-v4";
+
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("pathasala-store").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js"
-      ]);
-    })
-  );
+    self.skipWaiting();
+
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./style.css",
+                "./app.js"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        fetch(e.request)
+            .then((response) => {
+                return response;
+            })
+            .catch(() => {
+                return caches.match(e.request);
+            })
+    );
 });
